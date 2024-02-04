07e32a86e2dd14b94f96bec08a8167f16768e702
Ayan <kuspanov.2002@gmail.com>
2024-02-04 21:04:13 +0600

    task 1

---
+++ pp2/TSIS3/Classes/task 1.py
@@ -0,0 +1,15 @@
+class ConsoleIO:
+    line: str = "None"
+
+    def getString():
+        return input("Please, write something!")
+    
+    def printString():
+        return print(line.upper())
+    
+consoleIO = ConsoleIO()
+consoleIO.getString()
+consoleIO.printString()
+
+print("LOL!")
+    
