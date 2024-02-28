19271a64444a3bf14d92f32777b37549e186252a
PuckInside <kuspanov.2002@gmail.com>
2024-02-28 18:33:02 +0600

    Complite TSIS5

--- pp2/TSIS4/PythonDate/SecDiff.py
+++ pp2/TSIS4/PythonDate/SecDiff.py
@@ -1,7 +1,7 @@
 import datetime
 
-firstDate = datetime.datetime(2022, 5, 13, 0, 0, 0)
-secondDate = datetime.datetime(2025, 9, 9, 0, 0, 0)
+firstDate = datetime.datetime(2022, 5, 13)
+secondDate = datetime.datetime(2077, 5, 13)
 diff = secondDate - firstDate
 diffInSec = diff.total_seconds()
 print("The difference between the two dates is", diffInSec, "seconds.")
